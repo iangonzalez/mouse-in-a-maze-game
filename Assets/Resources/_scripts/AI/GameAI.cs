cde373fc84f0028e66d2ceabcb806dabb0f74d80
Ian Gonzalez <gonzalez.isv@gmail.com>
2016-03-08 22:40:45 -0500

    added mechanism for action selection in ai

--- Assets/Resources/_scripts/AI/GameAI.cs
+++ Assets/Resources/_scripts/AI/GameAI.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Reflection;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public enum AICommunicationState {
     NotInCommuncation,
     InCommunication
+}
+
+public enum AIAlignmentState {
+    Neutral,
+    Friendly,
+    VeryFriendly,
+    Hostile,
+    VeryHostile
 }
 
 public class GameAI : MonoBehaviour {
@@ -22,6 +31,7 @@ public class GameAI : MonoBehaviour {
     private IntVector2 playerCurrentCoords;
 
     private AICommunicationState aiCommState;
+    private AIAlignmentState aiAlignmentState;
 
     private PlayerResponse playerResponse;
 
@@ -29,6 +39,8 @@ public class GameAI : MonoBehaviour {
     private bool firstInterchangeDone = false;
 
     private AiPlayerInterchange currentInterchange;
+
+    private Dictionary<AIAlignmentState, List<Action>> perStateActionList;
 
     private void Start() {
 
@@ -41,8 +53,66 @@ public class GameAI : MonoBehaviour {
 
         //start out not in communcation
         aiCommState = AICommunicationState.NotInCommuncation;
+        aiAlignmentState = AIAlignmentState.Neutral;
+
+        //initialize list of possible actions (for each state)
+        InitializeActionLists();
     }
 
+    //code to intialize the action lists for each state based on the AI's player-affecting methods.
+    //consider refactoring this into a superclass that this derives from.
+    #region
+    //helper function to convert a list of methodInfo objects into Actions
+    private List<Action> GetActionListFromMethodInfos(IEnumerable<MethodInfo> methodInfos) {
+        //Action.
+        return new List<Action> (methodInfos.Select(m => (Action)Delegate.CreateDelegate(typeof(Action), this, m, false)));
+    }
+
+    //helper function to filter a list of MethodInfo objects by name
+    private IEnumerable<MethodInfo> FilterMethodInfosByNameStart(IEnumerable<MethodInfo> methodInfos, string nameStart) {
+        return methodInfos.Where(m => m.Name.StartsWith(nameStart));
+    }
+
+    /// <summary>
+    /// Initialize the action lists for each alignment state. This gets every 0-param, void-returning method in the object
+    /// that starts with the state's name (plus "_") and puts it in a list for that state.
+    /// Neutral actions are added to every state.
+    /// Note that ALL methods designated as state actions with a State + _ name must be void return type and take no arguments for this to work.
+    /// </summary>
+    private void InitializeActionLists() {
+        perStateActionList = new Dictionary<AIAlignmentState, List<Action>>();
+
+        //get the methods of this type
+        var aiMethods = typeof(GameAI).GetMethods(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+        Debug.Log(aiMethods.Length);
+
+        //get the actions for Neutral first
+        var neutralMethods = FilterMethodInfosByNameStart(aiMethods, "Neutral_");
+        Debug.Log(neutralMethods.Count());
+        var neutralActions = GetActionListFromMethodInfos(neutralMethods);
+        Debug.Log(neutralActions.Count);
+        
+        foreach (AIAlignmentState state in Enum.GetValues(typeof(AIAlignmentState))) {
+            string stateName = state.ToString();
+            
+            //Neutral gets only neutral actions
+            if (state == AIAlignmentState.Neutral) {
+                perStateActionList[state] = neutralActions;
+            }
+            //all other states get actions that start with their name and Neutral
+            else {
+                var stateActions = GetActionListFromMethodInfos(FilterMethodInfosByNameStart(aiMethods, stateName + "_"));
+                perStateActionList[state] = new List<Action>(neutralActions.Concat(stateActions));
+            }
+            
+        }
+    }
+    #endregion
+
+
+    //Main business logic. Contains update function (called every frame), code to initiate actions/communications
+    //with player, handle responses, and change alignment states
+    #region
     private void Update() {
         //if in communcation, check for response, call handler on response if there
         if (aiCommState == AICommunicationState.InCommunication) {
@@ -65,66 +135,18 @@ public class GameAI : MonoBehaviour {
         else if (playerCurrentCoords != player.MazeCellCoords) {
             playerCurrentCoords = player.MazeCellCoords;
             if (!firstInterchangeDone) {
-                AskPlayerToTouchCorners(true);
+                Neutral_AskPlayerToTouchCorners();
                 firstInterchangeDone = true;
             }
-            else if (UnityEngine.Random.Range(0f, 1.0f) > 0.5) {
-                AskPlayerToTouchCorners();                
-            }
             else {
-                MakeTextRequestToPlayer();
+                var possibleActions = perStateActionList[aiAlignmentState];
+                int randIdx = UnityEngine.Random.Range(0, possibleActions.Count);
+                Action randAction = possibleActions[randIdx];
+                randAction();
             }
         }
 
         //Debug.Log(aiCommState.ToString());
-    }
-
-    private PlayerPath GetPlayerCornerPath() {
-        Vector3 localRoomPos = maze.GetCellLocalPosition(playerCurrentCoords.x, playerCurrentCoords.z);
-
-        var pointList = new List<Vector3> {
-            localRoomPos + new Vector3(0.5f, 0, 0.5f),
-            localRoomPos + new Vector3(-0.5f, 0, 0.5f),
-            localRoomPos + new Vector3(0.5f, 0, -0.5f),
-            localRoomPos + new Vector3(-0.5f, 0, -0.5f),
-        };
-        
-        return new PlayerPath(pointList, initWithListOrder: false);
-    }
-
-    public void AskPlayerToTouchCorners(bool firstRequest = false) {
-        var pathToFollow = GetPlayerCornerPath();
-        var cornerInterchange = new TouchCornersInterchange(new PlayerResponse(pathToFollow, false), firstRequest);
-        RequestPlayerToFollowPath(cornerInterchange, roomExitCommChannel);
-    }
-
-    private void RequestPlayerToFollowPath(PathInterchange pathInterchange, PathCommuncationChannel channel) {
-        currentInterchange = pathInterchange;
-        channel.SetPathForPlayer(pathInterchange.expectedResponse.playerPath);
-        SendMessageToPlayer(pathInterchange.GetQuestionText(), channel);
-        
-    }
-
-    public void MakeTextRequestToPlayer() {
-        maze.CloseDoorsInCell(playerCurrentCoords);
-        currentInterchange = new TextOnlyInterchange();
-        SendMessageToPlayer(currentInterchange.GetQuestionText(), textCommChannel);
-    }
-    
-
-    /// <summary>
-    /// sends the given string message to the player via the given channel. 
-    /// Also sets current channel to be given channel.
-    /// </summary>
-    /// <param name="message"></param>
-    /// <param name="channel"></param>
-    private void SendMessageToPlayer(string message, CommunicationChannel channel) {
-        if (channel == null) {
-            throw new Exception("channel was null");
-        }
-        aiCommState = AICommunicationState.InCommunication;
-        currentCommChannel = channel;
-        channel.StartCommunicationWithPlayer(player, this, message);        
     }
 
     /// <summary>
@@ -142,4 +164,68 @@ public class GameAI : MonoBehaviour {
         bool wasResponseCorrect = currentInterchange.CheckIfCorrectResponse(response);
         SendMessageToPlayer(currentInterchange.GetResponseToPlayerText(wasResponseCorrect), oneWayCommChannel);
     }
+    #endregion
+
+    // Ai player communcation helper functions. Functions that handle communicating with player through the AI's
+    // communcation channels.
+    #region
+    private PlayerPath GetPlayerCornerPath() {
+        Vector3 localRoomPos = maze.GetCellLocalPosition(playerCurrentCoords.x, playerCurrentCoords.z);
+
+        var pointList = new List<Vector3> {
+            localRoomPos + new Vector3(0.5f, 0, 0.5f),
+            localRoomPos + new Vector3(-0.5f, 0, 0.5f),
+            localRoomPos + new Vector3(0.5f, 0, -0.5f),
+            localRoomPos + new Vector3(-0.5f, 0, -0.5f),
+        };
+
+        return new PlayerPath(pointList, initWithListOrder: false);
+    }
+
+    private void RequestPlayerToFollowPath(PathInterchange pathInterchange, PathCommuncationChannel channel) {
+        currentInterchange = pathInterchange;
+        channel.SetPathForPlayer(pathInterchange.expectedResponse.playerPath);
+        SendMessageToPlayer(pathInterchange.GetQuestionText(), channel);
+
+    }
+
+    /// <summary>
+    /// sends the given string message to the player via the given channel. 
+    /// Also sets current channel to be given channel.
+    /// </summary>
+    /// <param name="message"></
+    /// <param name="channel"></param>
+    private void SendMessageToPlayer(string message, CommunicationChannel channel) {
+        if (channel == null) {
+            throw new Exception("channel was null");
+        }
+        aiCommState = AICommunicationState.InCommunication;
+        currentCommChannel = channel;
+        channel.StartCommunicationWithPlayer(player, this, message);
+    }
+    #endregion
+
+
+    /// <summary>
+    /// Below this point in the code are the AI's state specific high-level actions. The ai chooses between these
+    /// actions randomly depending on its current alignment state.
+    /// </summary>
+
+    // Neutral AI actions
+    #region
+    private void Neutral_AskPlayerToTouchCorners() {
+        var pathToFollow = GetPlayerCornerPath();
+        var cornerInterchange = new TouchCornersInterchange(new PlayerResponse(pathToFollow, false), 
+                                                            !firstInterchangeDone);
+        RequestPlayerToFollowPath(cornerInterchange, roomExitCommChannel);
+    }
+
+    private void Neutral_MakeTextRequestToPlayer() {
+        maze.CloseDoorsInCell(playerCurrentCoords);
+        currentInterchange = new TextOnlyInterchange();
+        SendMessageToPlayer(currentInterchange.GetQuestionText(), textCommChannel);
+    }
+    #endregion
+
+    
 }
